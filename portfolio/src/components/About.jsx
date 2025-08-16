f06d787e2b010748a3165131f6ab394382a60184
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 12:50:52 -0400

    Shorten About text

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -4,12 +4,8 @@ export const About = () => {
   return (
     <div className='about-me-container'>
         <h2 className='about-me_h2'>about</h2>
-        <p className='about-me_text'> I'm a multi-faceted full-stack web developer 
-            with experience creating websites from scratch
-            or using Shopify templates, as well as a passion
-            for design, digital and traditional art, animation,
-            photo editing, game development, and social media 
-            management... to name a few things. :)
+        <p className='about-me_text'>
+          Where art meets technology
         </p>
     </div>
   )
