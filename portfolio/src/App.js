8cfc9b2589f3cf5539a6693619b094f17f4e87b1
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 01:04:16 -0400

    Create a new component for rendering all logos

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
+import { Logos } from './components/Logos';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logos />
         
       </header>
     </div>
---
+++ portfolio/src/components/Logos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import reactLogo from './logos/react-logo.svg';
+
+export const Logos = () => {
+  return (
+    <div>
+        <img src={reactLogo} className="App-logo" alt="React logo" />
+    </div>
+  )
+}
