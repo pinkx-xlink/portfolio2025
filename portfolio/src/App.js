64ff1fe8a9441103a749e4e7064c63dd07008bb8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 01:46:40 -0400

    Import About.jsx

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Header from './components/Header';
 import { Logos } from './components/Logos';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <About />
         <Logos />
         
       </header>
