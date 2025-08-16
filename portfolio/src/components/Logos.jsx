2a16fea33ae6e51a2fd9a97ae61232f430549ceb
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 01:44:00 -0400

    Rearrange the logos using flex and grid layout methods

--- portfolio/src/components/Logos.jsx
+++ portfolio/src/components/Logos.jsx
@@ -9,28 +9,33 @@ import sqlLogo from './logos/sql-logo.png';
 
 export const Logos = () => {
   return (
-    <>
+    <div className='logos-container'>
+        <div className='logos-container__html-css-and-js-container'>
+            <div>
+                <img src={javascriptLogo} className="App-logo" alt="JavaScript logo" />
+            </div>
+            <div>
+                <img src={htmlLogo} className="App-logo" alt="JavaScript logo" />
+            </div>
+            <div>
+                <img src={cssLogo} className="App-logo" alt="JavaScript logo" />
+            </div>
+        </div>
+        
         <div>
             <img src={reactLogo} className="animated-logo" alt="React logo" />
         </div>
-        <div>
-            <img src={javascriptLogo} className="App-logo" alt="JavaScript logo" />
-        </div>
-        <div>
-            <img src={htmlLogo} className="App-logo" alt="JavaScript logo" />
-        </div>
-        <div>
-            <img src={cssLogo} className="App-logo" alt="JavaScript logo" />
-        </div>
-        <div>
-            <img src={nodeJsLogo} className="App-logo" alt="JavaScript logo" />
-        </div>
+        
         <div>
             <img src={powerBiLogo} className="App-logo" alt="JavaScript logo" />
         </div>
         <div>
             <img src={sqlLogo} className="App-logo" alt="JavaScript logo" />
         </div>
-    </>
+
+        <div>
+            <img src={nodeJsLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+    </div>
   )
 }
