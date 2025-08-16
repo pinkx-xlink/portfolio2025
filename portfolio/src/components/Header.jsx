2e2774993323d0fbb9b48e489b04611a3492591e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 00:52:15 -0400

    Create components folder, and create the Header component within it; write the Header function, and call it in App.js

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='homepage__header'>
+        <p>hi!</p>
+        <span> 
+            <p>I'm</p>
+            <h1>Shaiyan Younkin</h1>
+        </span>
+    </div>
+  )
+}
+
+export default Header
