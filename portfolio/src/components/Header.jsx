ebefc8597a360487fe3a9648058450d0afcf1807
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 02:38:05 -0400

    Add classNames for text elements, and update text colors

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='homepage__header-container'>
-        <p>hi!</p>
+        <p className='header__hi'>hi!</p>
         <span className='homepage__header-container_greeting'> 
             <p>I'm</p>
-            <h1>Shaiyan Younkin</h1>
+            <h1 className='full-name'>Shaiyan Younkin</h1>
         </span>
     </div>
   )
