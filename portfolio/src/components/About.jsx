0a5f6bfeda91e7dcf80336bd20974fd656e57f66
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 02:54:49 -0400

    Add classNames, background color, and borders to About elements

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>
-        <h2>about</h2>
-        <p>I'm a multi-faceted full-stack web developer 
+    <div className='about-me-container'>
+        <h2 className='about-me_h2'>about</h2>
+        <p className='about-me_text'> I'm a multi-faceted full-stack web developer 
             with experience creating websites from scratch
             or using Shopify templates, as well as a passion
             for design, digital and traditional art, animation,
