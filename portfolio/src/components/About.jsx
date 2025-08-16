b4c57a65b665979bf2642698442ccc1581c23669
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 01:50:26 -0400

    Add "about" bio

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -4,7 +4,13 @@ export const About = () => {
   return (
     <div>
         <h2>about</h2>
-        <p>filler text blah blah blah.....</p>
+        <p>I'm a multi-faceted full-stack web developer 
+            with experience creating websites from scratch
+            or using Shopify templates, as well as a passion
+            for design, digital and traditional art, animation,
+            photo editing, game development, and social media 
+            management... to name a few things. :)
+        </p>
     </div>
   )
 }
