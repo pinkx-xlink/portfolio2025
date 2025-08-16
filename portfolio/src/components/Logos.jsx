44675887b98a5c5390dd354a9786094565e5832d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-16 01:31:51 -0400

    Add software and programming language logos

--- portfolio/src/components/Logos.jsx
+++ portfolio/src/components/Logos.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import reactLogo from './logos/react-logo.svg';
+import javascriptLogo from './logos/javascript-logo.png';
+import htmlLogo from './logos/html-logo.png'
+import cssLogo from './logos/css-logo.png'
+import nodeJsLogo from './logos/nodejs-logo.png';
+import powerBiLogo from './logos/power-bi-logo.png';
+import sqlLogo from './logos/sql-logo.png';
 
 export const Logos = () => {
   return (
-    <div>
-        <img src={reactLogo} className="App-logo" alt="React logo" />
-    </div>
+    <>
+        <div>
+            <img src={reactLogo} className="animated-logo" alt="React logo" />
+        </div>
+        <div>
+            <img src={javascriptLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={htmlLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={cssLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={nodeJsLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={powerBiLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={sqlLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+    </>
   )
 }
