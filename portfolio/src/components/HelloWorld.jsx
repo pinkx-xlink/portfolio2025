567491b416fd1f1339c6683cbb6f9303f9bc8619
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-17 01:48:33 -0400

    Comment out code for ↓ neon sign element

--- portfolio/src/components/HelloWorld.jsx
+++ portfolio/src/components/HelloWorld.jsx
@@ -6,13 +6,13 @@ export const HelloWorld = () => {
     <>
         <div className='neon-signs-container'>
         <div className='hello-world' contenteditable spellcheck="false">Hello, <br/>World</div>
-        <div className='neon-sign__i-am'>
+        {/* <div className='neon-sign__i-am'>
                 <span>I'm</span>
                 <span> </span>
                 <span> </span>
                 <span> </span>
                 <div class="scroll-more">↓</div>
-        </div>
+        </div> */}
         </div>
     </>
   
