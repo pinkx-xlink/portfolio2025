e08f99f61cc1ada58a60a5347bceb87cf2807083
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-17 01:44:55 -0400

    Remove animation from Logos; move them into the sidebar, in a vertical line

--- portfolio/src/components/Logos.jsx
+++ portfolio/src/components/Logos.jsx
@@ -18,36 +18,31 @@ animate('.App-logo', {
 export const Logos = () => {
   return (
     <div className='logos-container'>
-        <div className='logo-group-1'>
-            <div>
-                <img src={javascriptLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
-            <div>
-                <img src={htmlLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
-            <div>
-                <img src={cssLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
+        <div>
+            <img src={javascriptLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={htmlLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={cssLogo} className="App-logo" alt="JavaScript logo" />
         </div>
         
-        <div className='logo-group-2'>
-            <div>
-                <img src={reactLogo} className="App-logo" id="animated-logo" alt="React logo" />
-            </div>
-
-            <div>
-                <img src={nodeJsLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
-
-
-            <div>
-                <img src={powerBiLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
-            <div>
-                <img src={sqlLogo} className="App-logo" alt="JavaScript logo" />
-            </div>
+        <div>
+            <img src={reactLogo} className="App-logo" id="animated-logo" alt="React logo" />
         </div>
-       
+
+        <div>
+            <img src={nodeJsLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+
+
+        <div>
+            <img src={powerBiLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
+        <div>
+            <img src={sqlLogo} className="App-logo" alt="JavaScript logo" />
+        </div>
     </div>
   )
 }
