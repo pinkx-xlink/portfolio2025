b94eaa9173487411a1046e76a2ca4d7c0256a748
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-19 10:59:27 -0400

    Update page title

--- portfolio/src/components/AnimatedGreeting.jsx
+++ portfolio/src/components/AnimatedGreeting.jsx
@@ -15,7 +15,7 @@ const AnimatedGreeting = () => {
         <div>
             {/* <p>I'm</p> */}
             <h1 className="entrance-animation" id="header__animated-h1">I'm Shaiyan Younkin
-              <p> web dev - move this el out of h1 and change the font and color</p>
+              <p> web dev </p>
             </h1>
             
           </div>
