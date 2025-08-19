568f974c216833d475d230bb9bec08286c0ce799
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-19 15:42:23 -0400

    Add link to repository

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+        <p>
+            <b>Wanna see behind-the-scenes?</b>
+            Check out the repo for my portfolio here:
+            <a href="https://github.com/pinkx-xlink/portfolio2025" target='_blank'>Portfolio Repository</a>
+        </p>
+    </div>
   )
 }
