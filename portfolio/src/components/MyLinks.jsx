63c70f20ec0701e51aa4b29b880be2b7a15e7f0b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-19 12:09:25 -0400

    Update background color and content alignment for my-links

--- portfolio/src/components/MyLinks.jsx
+++ portfolio/src/components/MyLinks.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 
+import githubLogo from './logos/GitHub-Logo.png';
+import linkedinLogo from './logos/linkedin-logo.webp';
+
 export const MyLinks = () => {
   return (
     <div className='my-links-container'>
-        <a 
-        href="https://github.com/pinkx-xlink" 
-        target="_blank"
-        className="my-links">
-        Github
-        </a>
+      <div className='my-link'>
+         <img src={githubLogo} className='my-links__logos'></img>
+          <a 
+          href="https://github.com/pinkx-xlink" 
+          target="_blank"
+          className="my-links">
+          Github
+          </a>
+      </div>
+
+      <div className='my-link'>
+        <img src={linkedinLogo} className='my-links__logos'></img>
         <a 
         href="https://www.linkedin.com/in/shaiyan-younkin-916633271/" 
         target="_blank"
         className="my-links">
         LinkedIn
         </a>
+      </div>
+        
     </div>
   )
 }
