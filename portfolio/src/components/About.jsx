1040c624f4b8f9de094ce0b210855176fd318107
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-19 11:47:54 -0400

    Update about-me_h2

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div className='about-me-container'>
-      <p className='about-me_h2'>about</p>
+      <p className='about-me_h2'> {'<'} web dev {'>'}</p>
       <p className='about-me_text'>
         Where art meets technology
       </p>
