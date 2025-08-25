de8477df292934777bdac985bb3543ded17187d4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-25 13:48:49 -0400

    Edit the About text; decrease height of Logos-container and add top padding to it

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -7,8 +7,11 @@ export const About = () => {
     <div className='about-me-container'>
       <p className='about-me_h2'> {'<'} web dev {'>'}</p>
       <p className='about-me_text'>
-        Where art meets technology
-
+        I'm a full-stack web developer with a particular passion
+        for front-end development.
+        <br/>
+        <br/>
+        HTML ✨ JS ✨ CSS ✨ Node.js ✨ React ✨ Power BI ✨ SQL
         <br/>
         <br/>
         <br/>
