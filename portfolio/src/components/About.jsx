c814e809d030ab5d20bd6eca27699e5347ec43a9
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-25 19:27:45 -0400

    Update About

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -5,10 +5,11 @@ import { Logos } from './Logos';
 export const About = () => {
   return (
     <div className='about-me-container'>
-      <p className='about-me_h2'> {'<'} web dev {'>'}</p>
+      <p className='about-me_h2'> 
+        {'<'} full-stack web dev {'>'}
+      </p>
       <p className='about-me_text'>
-        I'm a full-stack web developer with a particular passion
-        for front-end development.
+        Hi, I'm Shaiyan! Full-time artist, cat-mom, reader, fashion designer, seamtress... oh yeah, and sometimes a programmer. 
         <br/>
         <br/>
         Here are some of my skills:
@@ -17,11 +18,10 @@ export const About = () => {
         HTML ✨ JS ✨ CSS ✨ Node.js ✨ React ✨ Power BI ✨ SQL
         <br/>
         <br/>
-        <br/>
-
         <div className='Logos-container'>
           <Logos />
         </div>
+        <br/>
       </p>
       
     </div>
