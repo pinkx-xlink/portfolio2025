991165af6bda03713c8c8d70528809ebf13d03ff
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-26 13:40:46 -0400

    Import all screenshots

--- portfolio/src/components/PowerBIProject/PowerBIProject.js
+++ portfolio/src/components/PowerBIProject/PowerBIProject.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 import './PowerBIProject.css';
-import projectScreenshot1 from './img/dashboard-screenshot3.png';
+import projectScreenshot1 from './img/dashboard-screenshot.png';
+import projectScreenshot2 from './img/dashboard-screenshot1.png';
+import projectScreenshot3 from './img/dashboard-screenshot2.png';
+import projectScreenshot4 from './img/dashboard-screenshot3.png';
+import projectScreenshot5 from './img/dashboard-screenshot4.png';
+import projectScreenshot6 from './img/dashboard-screenshot5.png';
+import projectScreenshot7 from './img/dashboard-screenshot6.png';
+import projectScreenshot8 from './img/dashboard-screenshot7.png';
+import projectScreenshot9 from './img/dashboard-screenshot8.png';
+import projectScreenshot10 from './img/dashboard-screenshot9.png';
+import projectScreenshot11 from './img/dashboard-screenshot10.png';
+
+
 
 export default function PowerBIProject() {
   return (
