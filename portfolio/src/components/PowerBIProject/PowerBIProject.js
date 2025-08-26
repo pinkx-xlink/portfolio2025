e3ef500df64bf447436e977e6f774a07c3aa18f7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-26 12:27:20 -0400

    Create img folder within PowerBiProject folder; move the project screenshot into the new img folder

--- portfolio/src/components/PowerBIProject/PowerBIProject.js
+++ portfolio/src/components/PowerBIProject/PowerBIProject.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import {projectScreenshot1} from '/img/dashboard-screenshot3.png';
+
 export default function PowerBIProject() {
   return (
-    <h2>PowerBIProject</h2>
+    <>
+    
+      <h2>PowerBIProject</h2>
+
+      <div className='project-images__container'>
+        <div className='project-image__item'>
+          <img src={projectScreenshot1} alt=" "/>
+        </div>
+      </div>
+
+    </>
   )
 }
