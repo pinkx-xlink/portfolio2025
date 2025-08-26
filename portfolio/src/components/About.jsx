e47ad5121d518dacf1667678d15750cb8a363236
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-26 16:12:23 -0400

    Update About text, and change the font size and justify the text.

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -15,11 +15,23 @@ export const About = () => {
         I love creating unique, eye-catching, and user-friendly
         apps that are memorable and easy to navigate. I'm also passionate
         about game development and animation.
+
+        In my free time, I create new apps from my own
+        imagination and follow tutorials and courses
+        to expand my horizons and learn more advanced coding methods.
+        
         <br/>
         <br/>
-        In my free time, I love creating new apps from my own
-        imagination, as well as following tutorials and courses
-        to expand my horizons and learn more advanced methods.
+        I'm currently seeking opportunities where I can put
+        to use and refine my current web development skills,
+        as well as add new tools to my kit.
+        <br/>
+        <br/>
+        5 years from now, I see myself in a flexible ever-evolving role that will allow me
+        to work on a wide range of exciting projects, from creating the latest hottest
+        social media app, to implementing physics for the next Elder Scrolls game.
+        <br/>
+        <br/>
         My skills include:
         <br/>
         <p className='skills'>°˖ꕥ HTML ꕥ JS ꕥ CSS ꕥ Node.js ꕥ React ꕥ Power BI ꕥ SQL ꕥ˖°</p>
