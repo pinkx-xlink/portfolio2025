24f0d265665ec25698554d1fd67628a3a2285445
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-08-26 13:33:28 -0400

    Add screenshots of Power BI dashboard project

--- portfolio/src/components/PowerBIProject/PowerBIProject.js
+++ portfolio/src/components/PowerBIProject/PowerBIProject.js
@@ -1,16 +1,76 @@
 import React from 'react';
 
+import './PowerBIProject.css';
 import projectScreenshot1 from './img/dashboard-screenshot3.png';
 
 export default function PowerBIProject() {
   return (
     <>
-    
-      <h2>PowerBIProject</h2>
+      <div className='project__container'>
+        <h2>Power BI Dashboard</h2>
 
-      <div className='project-images__container'>
-        <div className='project-image__item'>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
           <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
+        </div>
+        <div className='project__item'>
+          <h3>Test Scores Data Analytics</h3>
+          <img src={projectScreenshot1} alt=" "/>
+          <p className='project__item__p-text'>filler text filler filler</p>
         </div>
       </div>
 
